a0a31c507cc4253652dc1e1d95c4928c5d071ae5
shaker <shakalghaithi@gmail.com>
2014-06-20 14:29:50 -0700

    just slightly changes

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,35 +1,75 @@
 class PlansController < ApplicationController
-  #before_action :authenticate_user!
-  before_action :find_plan, only: [:show, :edit, :update]
+  before_action :authenticate_user!
+  before_action :set_plan, only: [:show, :edit, :update, :destroy]
+
+  # GET /plans
+  # GET /plans.json
   def index
-    @plans = Plan.all
+    @plans = Plan.all.paginate(:page => params[:page], :per_page => 5)
   end
+
+  # GET /plans/1
+  # GET /plans/1.json
+  def show
+  end
+
+  # GET /plans/new
   def new
     @plan = Plan.new
   end
-  def create
-    @plan = Plan.new plan_params
-    @plan.save
-    redirect_to plans_path
-  end
-  def show
-   @plan = @plan.text
-  end
+
+  # GET /plans/1/edit
   def edit
   end
+
+  # POST /plans
+  # POST /plans.json
+  def create
+    @plan = Plan.new(plan_params)
+
+    respond_to do |format|
+      if @plan.save
+        format.html { redirect_to @plan, notice: 'Plan was successfully created.' }
+        format.json { render :show, status: :created, location: @plan }
+      else
+        format.html { render :new }
+        format.json { render json: @plan.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /plans/1
+  # PATCH/PUT /plans/1.json
   def update
-    @plan.update plan_params
-    redirect_to root_path
+    respond_to do |format|
+      if @plan.update(plan_params)
+        format.html { redirect_to @plan, notice: 'Plan was successfully updated.' }
+        format.json { render :show, status: :ok, location: @plan }
+      else
+        format.html { render :edit }
+        format.json { render json: @plan.errors, status: :unprocessable_entity }
+      end
+    end
   end
+
+  # DELETE /plans/1
+  # DELETE /plans/1.json
   def destroy
-    Plan.find(params[:id]).destroy
-    redirect_to root_path
+    @plan.destroy
+    respond_to do |format|
+      format.html { redirect_to plans_url, notice: 'Plan was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
+
   private
-  def find_plan
-     @plan = Plan.find(params[:id])
-  end
-  def plan_params
-    params.require(:plan).permit(:text)
-  end
+    # Use callbacks to prevent reapetation.
+    def set_plan
+      @plan = Plan.find(params[:id])
+    end
+
+    # Only allow the parameters that we want.
+    def plan_params
+      params.require(:plan).permit(:contant)
+    end
 end
