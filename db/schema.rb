f9c6b59089270e1147cfbc85ee5e466fca853cb8
shaker <shakalghaithi@gmail.com>
2014-06-23 19:37:11 -0700

    added postgreql as db

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140619164053) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: true do |t|
     t.text     "text"
     t.integer  "plan_id"
@@ -20,7 +23,7 @@ ActiveRecord::Schema.define(version: 20140619164053) do
     t.datetime "updated_at"
   end
 
-  add_index "comments", ["plan_id"], name: "index_comments_on_plan_id"
+  add_index "comments", ["plan_id"], name: "index_comments_on_plan_id", using: :btree
 
   create_table "plans", force: true do |t|
     t.text     "contant"
@@ -43,7 +46,7 @@ ActiveRecord::Schema.define(version: 20140619164053) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
